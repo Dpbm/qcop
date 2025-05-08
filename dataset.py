ac737aaa302394e3dd884081c6778ffb8027ef69
Dpbm <dpbm136@gmail.com>
2025-05-08 14:27:47 -0300

    using polars

--- dataset.py
+++ dataset.py
@@ -1,13 +1,20 @@
+from typing import Dict, List, TypedDict
+from copy import deepcopy
+
+from qiskit import QuantumCircuit
 from qiskit.circuit.random import random_circuit
+from qiskit.transpiler import generate_preset_pass_manager, StagedPassManager
+
 from qiskit_aer import AerSimulator
 from qiskit_aer.primitives import Sampler
-from qiskit.transpiler import generate_preset_pass_manager
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 from multiprocessing.pool import Pool
 from tqdm import tqdm
-import pandas as pd
+import polars as pl
 import hashlib
 from PIL import Image
 import torch 
@@ -18,110 +25,141 @@ from itertools import product
 
 from constants import *
 from image import transform_image
+from colors import Colors
 
-def generate_image(sim, index, depth):
+Dist = Dict[int,float]
+States = List[int]
+class CircuitResult(TypedDict):
+    index:int
+    depth:int
+    file:str
+    result:str # JSON string
+    hash:str
+
+
+def generate_circuit(depth:int, circuit_image_path:str, pm:StagedPassManager) -> QuantumCircuit:
     qc = random_circuit(N_QUBITS, depth)
     qc.measure_all()
-    
-    filename = f'{index}-{depth}.jpeg'
-    circuit_image_path = os.path.join(DATASET_PATH, filename)
     qc.draw('mpl', filename=circuit_image_path)
 
-    with open(circuit_image_path, "rb") as file:
+    isa_qc = pm.run(qc)
+    return isa_qc
+
+def get_circuit_results(qc:QuantumCircuit, sampler:Sampler) -> Dist:
+    return sampler.run([qc], shots=SHOTS).result().quasi_dists[0]
+
+def fix_dist_gaps(dist:Dist, states:States):
+    for state in states:
+        result_value = result.get(state)
+        if result_value is None:
+            dist[state] = 0
+
+def generate_image(index:int, depth:int, pm:StagedPassManager, sampler:Sampler, states:States, image_path:str) -> CircuitResult:
+    print("%sGenerating circuit image: %d%s"%(Colors.MAGENTAFG,index,Colors.ENDC))
+
+    isa_qc = generate_circuit(depth, image_path)
+
+    with open(image_path, "rb") as file:
         file_hash = hashlib.md5(file.read()).hexdigest()
     
-    pm_aer = generate_preset_pass_manager(backend=sim, optimization_level=0)
-    aer_isa = pm_aer.run([qc])[0]
-
-    sampler = Sampler()
-    result = sampler.run([aer_isa], shots=SHOTS).result().quasi_dists[0]
-
-    for comb in product('01', repeat=N_QUBITS):
-        bitstring = ''.join(comb)
-        binary_to_dec = int(bitstring, 2)
-        result_value = result.get(binary_to_dec)
-        if result_value is None:
-            result[binary_to_dec] = 0
+    result = get_circuit_results(isa_qc, sampler)
+    fix_dist_gaps(result, states)
    
     return {
+        "index":index,
         "depth":depth,
         "file": filename,
         "result": json.dumps(list(result.values())),
         "hash": file_hash,
     }
 
-def generate_images():
-    index = 0 
-    df = pd.DataFrame(columns=("depth", "file", "result", "hash"))
+def generate_images() -> pl.DataFrame:
+    df = pl.DataFrame(schema={"index":pl.UInt16, "depth":pl.UInt8, "file":pl.String, "result":pl.String, "hash":pl.String})
+
+    sim = AerSimulator()
+    pm = generate_preset_pass_manager(backend=sim, optimization_level=0)
+    sampler = Sampler()
+
+    bitstrings_to_int = [ int(''.join(comb), 2) for comb in product('01', repeat=N_QUBITS) ]
 
     with tqdm(total=DATASET_SIZE)  as progress:
+
+        index = 0 
         while index < DATASET_SIZE:
+
             args = []
+
             for i in range(TOTAL_THREADS):
                 depth = np.random.randint(MIN_DEPTH, MAX_DEPTH)
-                sim = AerSimulator()
-                args.append((sim, index, depth))
+
+                filename = '{}-{}.jpeg'%(index,depth)
+                circuit_image_path = os.path.join(DATASET_PATH, filename)
+
+                args.append((index, depth, deepcopy(pm), deepcopy(sampler), bitstrings_to_int, circuit_image_path))
                 index += 1
 
             with Pool(processes=TOTAL_THREADS) as pool:
                 results = pool.starmap(generate_image, args)
             
             for result in results:
-                df.loc[len(df)] = result
+                tmp_df = pl.DataFrame(result)
+                df.vstack(tmp_df, in_place=True)
 
             progress.update(TOTAL_THREADS)
 
-    df.to_csv(DATASET_FILE, index=False)
+    return df
 
-def remove_duplicated_files():
-    csv_file = pd.read_csv(DATASET_FILE)
-    duplicated_lines = csv_file.loc[csv_file.duplicated(subset="hash", keep="first")]
+def remove_duplicated_files(df:pl.DataFrame) -> pl.DataFrame:
+    clean_df = df.unique(maintain_order=True, subset=["hash"])
+    clean_df_indexes = clean_df.get_column("index")
 
-    indexes = duplicated_lines.index.to_list()
-    files = [os.path.join(DATASET_PATH, file) for file in duplicated_lines["file"].to_list()]
-    
-    print("Dropping invalid rows")
-    csv_file = csv_file.drop(indexes)
-    print("Deleting duplicated files")
-    for file in tqdm(files):
+    duplicated_values = df.filter(~pl.col("index").is_in(clean_df_indexes))
+
+    print("%sDeleting duplicated files%s"%(Colors.GREENBG, Colors.ENDC))
+    for row in tqdm(pl.iter_rows(named=True)):
+        file = row["file"]
         os.remove(file)
+
+    return clean_df
     
-    csv_file.to_csv(DATASET_FILE, index=False)
     
-def transform_images(percentage_train=0.6):
-    print("Transforming images")
+def transform_images(df:pl.DataFrame, percentage_train:float=0.8):
+    print("%sTransforming images%s"%(Colors.GREENBG,Colors.ENDC))
+    print("%s%f For Training%s"%(Colors.YELLOWFG,percentage_train*100,Colors.ENDC))
+
     max_width,max_height = new_dim
 
-    files = os.listdir(DATASET_PATH)
-    total_files = len(files)
-    total_train_files = floor(total_files * percentage_train)
-    total_test_files = total_files - total_train_files
+    total_rows_training = int(len(df)*percentage_train)
+    total_rows_test = len(df)-total_rows_training
+    print("%s%d rows for training%s"%(Colors.YELLOWFG, total_rows_training, Colors.ENDC))
+    print("%s%d rows for testing%s"%(Colors.YELLOWFG, total_rows_test, Colors.ENDC))
 
     images_train = h5py.File(IMAGES_TRAIN, "w")
     images_test = h5py.File(IMAGES_TEST, "w")
 
-    for image_i in tqdm(range(total_train_files)):
-        image_path = os.path.join(DATASET_PATH, files[image_i])
-        
-        with Image.open(image_path) as img:
-            tensor = transform_image(img)
-            images_train.create_dataset(f"{image_i}", data=tensor)
+    train_rows = df.head(total_rows_training)
+    test_rows = df.tail(total_rows_test)
+    image_i = 0
+
+    for rows, h5_file in zip((train_rows, test_rows), (images_train, images_test)):
+        for row in tqdm(rows.iter_rows(named=True)):
+            image_path = row["file"]
+            with Image.open(image_path) as img:
+                tensor = transform_image(img)
+                h5_file.create_dataset(f"{image_i}", data=tensor)
+            image_i += 1
     
-    for image_i in tqdm(range(total_test_files)):
-        image_path = os.path.join(DATASET_PATH, files[total_train_files+image_i])
-        
-        with Image.open(image_path) as img:
-            tensor = transform_image(img)
-            images_test.create_dataset(f"{image_i}", data=tensor)
-
-
     images_train.close()
     images_test.close()
 
 def main():
     os.makedirs(DATASET_PATH, exist_ok=True)
-    generate_images()
-    remove_duplicated_files()
+
+    df = generate_images()
+    df = remove_duplicated_files(df)
+
+    df.write_csv(DATASET_PATH)    
+
     transform_images()
     
 
